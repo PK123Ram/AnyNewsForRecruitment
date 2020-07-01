20740da9d231bd400f863cbc39e72da46607c9b2
PK123Ram <kalyankranti96@gmail.com>
2020-07-01 11:46:23 +0530

    SplashScreen Design

---
+++ app/src/main/java/com/example/anynews/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.anynews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SystemClock.sleep(2000);
+        startActivity(new Intent(this,MainActivity.class ));
+    }
+}
